b33232a1ff4bda1b2245fd3b06620dcccf8adf5f
andriel300 <andrielscript@gmail.com>
2022-12-20 21:10:33 -0300

    add reset modern css

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import Tilt from "react-tilt";
 
 const Logo = () => {
-	return (
-		<div className='ma4 mt0'>
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 25 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner"> 👽 </div>
+      </Tilt>
+    </div>
+  );
+};
 
-		</div>
-	)
-}
-
-export default Logo
+export default Logo;
