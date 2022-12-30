c04a553867f9aba7a6b8fe377e51f68f6406c703
andriel300 <andrielscript@gmail.com>
2022-12-30 11:27:59 -0300

    add submit button

--- src/App.js
+++ src/App.js
@@ -7,13 +7,31 @@ import Particles from "./components/Particles/Particles";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         <Particles />
         {/*
         <FaceRecognition />} */}
