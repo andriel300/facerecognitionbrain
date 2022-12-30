eb1c2e13897af8ec72dbb875553744905dc795cf
andriel300 <andrielscript@gmail.com>
2022-12-30 19:48:40 -0300

    add calculate face location, calculate width and height of the imgs, change background colors, add face box blue colors

--- src/App.js
+++ src/App.js
@@ -12,8 +12,28 @@ class App extends Component {
     super();
     this.state = {
       input: "",
+      box: {},
     };
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = JSON.parse(data, null, 2).outputs[0].data.regions[0]
+      .region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
@@ -67,7 +87,9 @@ class App extends Component {
       requestOptions,
     )
       .then((response) => response.text())
-      .then((result) => console.log(result))
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response)),
+      )
       .catch((error) => console.log("error", error));
   };
 
@@ -82,7 +104,7 @@ class App extends Component {
           onButtonSubmit={this.onButtonSubmit}
         />
         <Particles />
-        <FaceRecognition imageUrl={this.state.input} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.input} />
       </div>
     );
   }
