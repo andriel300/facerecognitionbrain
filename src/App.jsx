11c5e9f7cb5c8234cd78faed16da46a1e4908c1e
andriel300 <andrielscript@gmail.com>
2023-01-26 10:48:48 -0300

    Add destructuring states, register problem where you dont loggin in register. add loadUser data state.

---
+++ src/App.jsx
@@ -0,0 +1,172 @@
+import React, { Component } from 'react';
+import Particles from './components/Particles/Particles';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: '',
+  },
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+    this.calculateFaceLocation = this.calculateFaceLocation.bind(this);
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        // password: data.password,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
+
+  calculateFaceLocation = (data) => {
+    const {
+      region_info: { bounding_box: clarifaiFace },
+    } = JSON.parse(data, null, 2).outputs[0].data.regions[0];
+    const image = document.getElementById('inputImage');
+    const { width, height } = image;
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box });
+  };
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onPictureSubmit = () => {
+    const USER_ID = 'andriel300';
+    const PAT = '9452072f14e143e18351e4681454cb29';
+    const APP_ID = 'my-first-application';
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+
+    const { input } = this.state;
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: input,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Key ${PAT}`,
+      },
+      body: raw,
+    };
+
+    fetch(
+      `https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`,
+      requestOptions,
+    )
+      .then((response) => response.text())
+      .then((responseText) => {
+        if (responseText) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(responseText));
+      })
+      .catch((error) => console.log('error', error));
+  };
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false });
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route });
+  };
+
+  render() {
+    const { isSignedIn, route, box, input } = this.state;
+    const { name, entries } = this.state.user;
+    return (
+      <div className="App">
+        <Particles id="tsparticles" />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+
+        {route === 'home' ? (
+          <div>
+            <Logo />
+            <Rank name={name} entries={entries} />
+
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onPictureSubmit={this.onPictureSubmit}
+            />
+
+            <FaceRecognition box={box} imageUrl={input} />
+          </div>
+        ) : route === 'signin' ? (
+          <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+        ) : (
+          <Register
+            onRouteChange={this.onRouteChange}
+            loadUser={this.loadUser}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
